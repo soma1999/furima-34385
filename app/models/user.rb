86433d0aeac095e9a12f43b8d333a68002b4e759
soma1999 <so12ma131999@gmail.com>
2021-02-03 14:12:21 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :firsy_name, :last_name, :first_name_katakana, :last_name_katakana, :birthday, presence: true  
 end
+ 

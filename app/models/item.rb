bfe7949ecc4ba081f9f4026036fc94c6a5f2baaa
soma1999 <so12ma131999@gmail.com>
2021-02-04 21:48:56 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
   
   belongs_to :user
+  has_one_attached :image
 
+  validates :title, :description , :category_id , :item_state_id , :shipping_fee_id, :prefecture_id, :shipping_day_id, presence: true, unless: :was_attached?
+
+  with_options presence: true, format: { with: [3-9][0-9]{2}|[1-9][0-9]{3,6} } do
+    validates :price
+  end
+
+  def was_attached?
+    self.image.attached?
+  end
 end

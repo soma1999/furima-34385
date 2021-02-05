ea2f26ebe7b1e29a5f6937177ee908f74bb38c2c
soma1999 <so12ma131999@gmail.com>
2021-02-05 11:04:41 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,20 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_state
+  belongs_to :prefecture
+  belongs_to :shipping_fee
+  belongs_to :shipping_day
+
   validates :title, :description , :category_id , :item_state_id , :shipping_fee_id, :prefecture_id, :shipping_day_id, presence: true, unless: :was_attached?
 
   with_options presence: true, format: { with: [3-9][0-9]{2}|[1-9][0-9]{3,6} } do
     validates :price
   end
+
+  validates :category_id , :item_state_id , :shipping_fee_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 } 
 
   def was_attached?
     self.image.attached?

4d953b536cd8bb4ecfcd0149e28eaf27b2db792f
soma1999 <so12ma131999@gmail.com>
2021-02-07 06:25:44 +0900

    before_action、updateアクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create] 
+  before_action :ensure_current_user, only: [:edit]
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -26,6 +27,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
@@ -40,5 +50,12 @@ class ItemsController < ApplicationController
       :price,
       :image
     ).merge(user_id: current_user.id)
+  end
+
+  def ensure_current_user
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id 
+      redirect_to action: :index
+    end
   end
 end

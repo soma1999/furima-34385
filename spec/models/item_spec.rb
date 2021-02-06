81d62fbf1422cddcb97288e8ba9c23960251103a
soma1999 <so12ma131999@gmail.com>
2021-02-06 10:21:31 +0900

    商品出品機能の変更２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,6 +32,35 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
 
+      it 'category_idが空では保存できないこと' do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'item_state_idが空では保存できないこと' do
+        @item.item_state_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item state can't be blank")
+      end
+
+      it 'shipping_fee_idが空では保存できないこと' do
+        @item.shipping_fee_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+      end
+
+      it 'prefecture_idが空では保存できないこと' do
+        @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'shipping_day_idが空では保存できないこと' do
+        @item.shipping_day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+
       it 'category_idが0では保存できないこと' do
         @item.category_id = 0
         @item.valid?

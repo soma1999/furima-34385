a6eed9800a95819f3f8c56da44e0bbfb0680f063
soma1999 <so12ma131999@gmail.com>
2021-02-08 12:47:17 +0900

    購入機能に必要なモデルやコントローラーの作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,29 @@
+class OrdersController < ApplicationController
+
+  def index
+    @user_order = UserOrder.new
+  end
+
+  def create
+    binding.pry
+    @user_order = UserOrder.new(order_params)
+    if @user_order.valid?
+      @user_order.save
+      redirect_to action: :index
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:user_order).permit(
+      :postal_code,
+      :prefecture_id,
+      :municipality,
+      :address,
+      :building_name,
+      :phone_number
+    )
+  end
+end

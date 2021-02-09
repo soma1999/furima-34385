0f3f74f43934edbca8830815bcc4cefc274a1288
soma1999 <so12ma131999@gmail.com>
2021-02-09 15:24:42 +0900

    ユーザーのURLからのアクセスの制限

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,8 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :pay_item, :forbid_seller_user, :forbid_sold_out]
+  before_action :forbid_unlogin_user
+  before_action :forbid_seller_user
+  before_action :forbid_sold_out
 
   def index
     @user_order = UserOrder.new
@@ -29,11 +33,33 @@ class OrdersController < ApplicationController
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
-    @item = Item.find(params[:item_id])
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],    
       currency: 'jpy'                 
     )
   end
+
+  def forbid_unlogin_user
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+
+  def forbid_seller_user
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def forbid_sold_out
+    if @item.order != nil
+      redirect_to root_path
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
